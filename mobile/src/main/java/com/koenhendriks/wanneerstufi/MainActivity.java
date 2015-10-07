e4be817edb076b3d1318ed405babb06b13ede453
Koen Hendriks <info@pixelfy.nl>
2015-10-07 11:47:04 +0200

    Added utils class and calculations between dates

--- mobile/src/main/java/com/koenhendriks/wanneerstufi/MainActivity.java
+++ mobile/src/main/java/com/koenhendriks/wanneerstufi/MainActivity.java
@@ -3,6 +3,15 @@ package com.koenhendriks.wanneerstufi;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +21,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        TextView text = (TextView) findViewById(R.id.date_untill);
+        Utils utils = new Utils();
+
+        Date sDate=null;
+        Date eDate=null;
+        Integer days = null;
+
+        try {
+            sDate = utils.dateFormat.parse("07/09/2015");
+            eDate = utils.dateFormat.parse("17/09/2015");
+            days = utils.daysDifference(sDate, eDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        if(sDate != null && eDate != null){
+            text.setText("Nog "+days+" dagen");
+        }else{
+            text.setText("Er is iets misgegaan");
+        }
+
     }
 }
---
+++ mobile/src/main/java/com/koenhendriks/wanneerstufi/Utils.java
@@ -0,0 +1,49 @@
+package com.koenhendriks.wanneerstufi;
+
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Wanneer Stufi
+ *
+ * Created by koen on 10/7/15.
+ */
+public class Utils {
+
+    public SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+    /**
+     * Get the amount of days between two dates.
+     *
+     * @param sDate Date of the first date to calculate
+     * @param eDate Date of the second date to calculate
+     * @return integer of the amount of days between the two days
+     */
+    public int daysDifference(Date sDate, Date eDate){
+
+        Calendar sCalendar = Calendar.getInstance();
+        sCalendar.setTime(sDate);
+
+        Calendar eCalendar = Calendar.getInstance();
+        eCalendar.setTime(eDate);
+
+        long milis1 = sCalendar.getTimeInMillis();
+        long milis2 = eCalendar.getTimeInMillis();
+
+        // Calculate difference in milliseconds
+        long diff = Math.abs(milis2 - milis1);
+
+        // Calculate difference in days
+        return (int)(diff / (24 * 60 * 60 * 1000));
+    }
+
+    public void nextPayDay(){
+
+    }
+
+
+}
